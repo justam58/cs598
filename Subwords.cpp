87abdade855a4bfa23e58e318279088484c178f3
Ping-ju Chen <justam@boxing.cs.byu.edu>
2013-09-24 13:09:20 -0600

    set up subwords

--- Subwords.cpp
+++ Subwords.cpp
@@ -4,51 +4,15 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-	int* inputs = new int[1024];
+	string target;
+	cin >> target;
 
-	int nums;
-	cin >> nums;
+	string word;
 
-	for(int i = 0; i < nums; i++){
-		long long int num;
-		cin >> num;
-		int index = 0;
-		while(num > 0){
-			int digit = num % 10;
-			index = index | (1 << digit);
-			if(index == 1023){
-				break;
-			}
-			num = num / 10;
-		}
-		inputs[index]++;	
-	}
+	int answer = 0;
 
-	long long int answer = 0;
-	for(int i = 0; i < 1024; i++){
-		if(inputs[i] == 0) {
-			continue;
-		}
-		for(int j = i; j < 1024; j++){
-			if(inputs[j] == 0 ){
-				continue;
-			}
-			if(i == j){
-				answer += inputs[i] * (inputs[j]-1) / 2;
-			}
-			else{
-				bool found = false;
-				for(int k = 0; k < 10; k++){
-					if(((i >> k) & 1 == 1) && ((j >> k) & 1 == 1)){
-						found = true;
-						break;
-					}
-				}
-				if(found){
-					answer += inputs[i] * inputs[j];
-				}
-			}
-		}
+	while(cin >> word){
+
 	}
 
 	cout << answer << endl;
