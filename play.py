a834643794962630268bdf9aebca0f9763247c15
Paula Chen <paula.chenn@gmail.com>
2013-12-11 17:56:53 -0700

    chnage milstone 2

--- play.py
+++ play.py
@@ -1,25 +1,25 @@
 import sys
 
-for line in sys.stdin:
-    all = set()
-    num = int(line)
+line = sys.stdin.readline()
+all = set()
+num = int(line)
 
-    count = 0
-    found = False
-    while(num not in all):
-        if num == 1:
-            found = True
-            break
-        all.add(num)
-        numStr = str(num)
-        num = 0
-        for ch in numStr:
-            v = int(ch)
-            num = num + v**2
-        count = count + 1
-    if found:
-        print count
+count = 0
+found = False
+while(num not in all):
+    if num == 1:
+        found = True
         break
+    all.add(num)
+    numStr = str(num)
+    num = 0
+    for ch in numStr:
+        v = int(ch)
+        num = num + v**2
+    count = count + 1
+if found:
+    print count
+else:
     print -1
 
 
